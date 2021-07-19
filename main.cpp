8c80de222446fcb6158331caad35c6eea3476b01
CosmicPegasis <aviralsingh@protonmail.com>
2021-07-19 10:41:51 +0530

    Write pseudocode

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+/* Pseudocode
+ 1. The Python List is implemented in various ways
+ 2. The plan is to use pointers that point to data
+ 3. These pointers will be stored in a vector
+ 4. Efficiency is not the goal
+ 5. All methods of the python list should be in the same time complexity as the original implementation
+ 6. Note down time complexity of every method and operator going forward
+ */
